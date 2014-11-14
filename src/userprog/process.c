637d022615b4fe5ca62cc1743894069a59d32127
ozensoydc <ozensoydc@uchicago.edu>
2014-11-14 14:59:02 -0600

    update process.c

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -17,7 +17,7 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
-
+#include "threads/malloc.h"
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
@@ -31,17 +31,24 @@ process_execute (const char *file_name)
   char *fn_copy;
   tid_t tid;
 
+  printf("in process_execute\n");
+  
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
   if (fn_copy == NULL)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
-
+  
+  printf("going to pass to start_process\n");
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
-  if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+  printf("just passed thread to start process\n");
+  
+  if (tid == TID_ERROR){
+    printf("tid error \n");
+    palloc_free_page (fn_copy);
+  } 
   return tid;
 }
 
@@ -50,6 +57,7 @@ process_execute (const char *file_name)
 static void
 start_process (void *file_name_)
 {
+  printf("in start_process: file name is %s\n",file_name_);
   char *file_name = file_name_;
   struct intr_frame if_;
   bool success;
@@ -60,7 +68,84 @@ start_process (void *file_name_)
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (file_name, &if_.eip, &if_.esp);
+  
+  char* save;
+  char* token;
+  int argc=0;
+  char* command_line=(char*)malloc(sizeof(strlen((char*)file_name)+1));
+  mempcpy(command_line,file_name_,strlen((char*)file_name)+1);
+  printf("command_line is: %s\n",command_line);
+  
+  int cum_length=0;
+  /*acquire argc*/
+  token=strtok_r(command_line," ",&save);
+  argc++;
+  while(token=strtok_r(NULL," ",&save)){
+    argc++;
+  }
+  /* arrays for argv and addresses*/
+  char *cmd_line=(char*)malloc(sizeof(strlen((char*)file_name)+1));
+  mempcpy(cmd_line,file_name_,strlen((char*)file_name)+1);
+  char** argv=(char **)malloc(sizeof(char)*(strlen((char*)file_name)+1));
+  int i=0;
+  int** addresses=(int **)malloc(sizeof(int*)*argc);
+  
+  token=strtok_r(cmd_line," ",&save);
+  printf("cmd_line is: %s\n",cmd_line);
+  argv[0]=token;
+  cum_length+=strlen(argv[0])+1;
+  
+  /* step argv array */
+  for(i=1;i<argc;i++){
+    argv[i]=strtok_r(NULL," ",&save);
+    printf("wrote %s to index %d in argv\n",argv[i],i);
+    cum_length+=strlen(argv[i])+1;
+  }
+  
+  /* push args into stack, and record their addresses */
+  int arg_len;
+  for(i=argc-1;i>=0;i--){
+    arg_len=strlen(argv[i])+1;
+    if_.esp -=arg_len;
+    addresses[i]=if_.esp;
+    mempcpy(if_.esp,argv[i],arg_len);
+  }
+  
+  /*word adjust */
+  int w_a=cum_length%4;
+  if(w_a!=0){
+    if_.esp-=4-w_a;
+  }
+  
+  /* NULL sentinel */
+  if_.esp-=4; //size of int
+  *(int*)if_.esp = 0;
+  
+  /*pushing arg addresses*/
+  for(i=argc-1;i>=1;i--){
+    if_.esp-=4;
+    *(void**)if_.esp=addresses[i];
+  }
 
+  /*argv*/
+  if_.esp-=4;
+  *(char **)if_.esp=if_.esp+4;
+
+  /*argc*/
+  if_.esp-=4;
+  *(int*)if_.esp=argc;
+  
+  /*fake return address*/
+  if_.esp-=4;
+  *(int*)if_.esp=0;
+
+  /*deallocate memory*/
+  free(argv);
+  free(addresses);
+  free(cmd_line);
+  free(command_line);
+  
+  
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
@@ -88,6 +173,8 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  while(1){
+  }
   return -1;
 }
 
@@ -215,6 +302,10 @@ load (const char *file_name, void (**eip) (void), void **esp)
   bool success = false;
   int i;
 
+  char* process_name=(char*)malloc(sizeof(strlen(file_name)+1));
+  mempcpy(process_name,file_name,strlen(file_name)+1);
+  printf("in load command line is %s: \n",process_name);
+  
   /* Allocate and activate page directory. */
   t->pagedir = pagedir_create ();
   if (t->pagedir == NULL) 
@@ -222,7 +313,10 @@ load (const char *file_name, void (**eip) (void), void **esp)
   process_activate ();
 
   /* Open executable file. */
-  file = filesys_open (file_name);
+  char* save_token;
+  process_name = strtok_r(process_name," ",&save_token);
+  printf("in load, process name is %s\n",process_name);
+  file = filesys_open (process_name);
   if (file == NULL) 
     {
       printf ("load: %s: open failed\n", file_name);
