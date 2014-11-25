411b8f549910ec0fb2597ca0b3ffc50f13c45e67
ozensoydc <ozensoydc@uchicago.edu>
2014-11-24 18:29:04 -0600

    updated exclude

--- src/vm/page.h
+++ src/vm/page.h
@@ -6,7 +6,7 @@
 #include "filesys/file.h"
 #include <stdbool.h>
 struct sup_page_table {
-  // should I include the thread?
+  // should I include the thread? trivial
   struct file *file;
   off_t ofs;
   //off_t bytes;
